47d40cb7274338bceedb4c2b81246f1f8c985282
Matt Wright <wright.matt92@gmail.com>
2015-10-03 21:25:37 -0500

    Initial file upload

---
+++ WebApplication1/Account/Login.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using WebApplication1.Models;
+
+namespace WebApplication1.Account
+{
+    public partial class Login : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}

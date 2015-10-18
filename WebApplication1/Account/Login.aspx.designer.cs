19bba950f26cfbfa7d781e34b6dac4269ffee4e9
Matt Wright <wright.matt92@gmail.com>
2015-10-17 20:25:43 -0500

    added missing files

--- WebApplication1/Account/Login.aspx.designer.cs
+++ WebApplication1/Account/Login.aspx.designer.cs
@@ -46,7 +46,7 @@ namespace WebApplication1.Account {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Login;
+        protected global::System.Web.UI.WebControls.Button BtnLogin;
         
         /// <summary>
         /// RememberMe control.
